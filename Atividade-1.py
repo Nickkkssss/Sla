d43929405d445aa9f5590111e58ed839ff3c2b1f
Nicks <nicolasdhminecraft@gmail.com>
2025-06-13 19:03:04 -0300

    Atividade-1.py
    Atividade 1 aula 3

---
+++ Atividade-1.py
@@ -0,0 +1,13 @@
+idade = 13
+minha_idade = "minha idade "
+#print minha_idade + str(idade)
+
+
+altura = " 1.60 "
+idade += float(altura)
+#print "minha altura" + altura + minha_idade + str(int(idade))
+
+
+altura = True
+idade = False
+print idade + altura
