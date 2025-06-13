bfcbc52be20deb5e1e7a83a387db02560dbd87bd
Nicks <nicolasdhminecraft@gmail.com>
2025-06-13 19:04:29 -0300

    atividade-3.py
    Atividade 2 aula 4

---
+++ atividade-3.py
@@ -0,0 +1,8 @@
+Flecha = 10
+VidaInimigo = 5
+if Flecha > VidaInimigo :
+  print "Inimigo perdeu"
+if Flecha < VidaInimigo :
+   print "Inimigo venceu"
+if Flecha == VidaInimigo :
+  print "Inimigo perdeu"
