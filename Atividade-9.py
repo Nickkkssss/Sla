0d59fd096763a2afad7edfe00374783bf5c75ab2
Nicks <nicolasdhminecraft@gmail.com>
2025-08-08 20:47:24 -0300

    Atividade-9.py
    Atividade 9 curso de python

---
+++ Atividade-9.py
@@ -0,0 +1,10 @@
+Nota1 = float(input ("nota= "))
+
+if Nota1 >= 9 :
+    print ("parabens sua nota foi um A")
+elif Nota1 >= 8 :
+    print ("parabens sua nota foi um B")
+elif Nota1 >= 7 :
+    print ("Sua nota foi um C")
+else:
+    print ("Sua nota foi um D")
