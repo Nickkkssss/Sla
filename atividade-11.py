f2d17f6fadb59b525c6bd819a7c47b60530ccb40
Nicks <nicolasdhminecraft@gmail.com>
2025-08-22 20:12:30 -0300

    atividade-11-2.py

--- atividade-11.py
+++ atividade-11.py
@@ -1,11 +1,17 @@
-numero = 8
-escolha = 10
-while escolha != numero :
-    escolha = float(input("escolha um numero entre 1 e 10: "))
+import random
+minimo = float(input("escolha o numero minimo: "))
+maximo = float(input("escolha o numero maximo: "))
+vida = float(input("escolha o numero de tentativas: "))
+numero = random.randint(int(minimo), int(maximo))
+escolha = 0
+while escolha != numero and vida > 0:
+    escolha = float(input("escolha um numero entre " + str(minimo) + " e " + str(maximo) + ": "))
     if escolha > numero:
         print ("Voce errou, seu numero foi maior")
     elif escolha < numero:
         print ("Voce errou, seu numero foi menor.")
     else :
         print ("voce acertou")
-    
+    vida -= 1
+    if vida == 0 and escolha != numero:
+        print ("Voce perdeu, suas tentativas acabaram")
