817a973ad3c1a366416e3d33c1ce9b08f7156a3b
Nicks <nicolasdhminecraft@gmail.com>
2025-09-10 19:59:46 -0300

    atividade-16.py

---
+++ atividade-16.py
@@ -0,0 +1,17 @@
+palavra = input("diga uma palavra: ")
+
+for letras in palavra:
+    if letras == "n":
+        print (letras + " faz parte do meu nome")
+    elif letras == "i":
+        print (letras + " faz parte do meu nome")
+    elif letras == "c":
+        print (letras + " faz parte do meu nome")
+    elif letras == "o":
+        print (letras + " faz parte do meu nome")
+    elif letras == "l":
+        print (letras + " faz parte do meu nome")
+    elif letras == "a":
+        print (letras + " faz parte do meu nome")
+    elif letras == "s":
+        print (letras + " faz parte do meu nome")
