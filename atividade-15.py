e7dfc89e6461b1d414479fca613efe581ace0347
Nicks <nicolasdhminecraft@gmail.com>
2025-09-10 19:54:46 -0300

    atividade-15.py

---
+++ atividade-15.py
@@ -0,0 +1,9 @@
+jogos = ["Hollow Knight", "Hello Neighbor ", "Don't starve Together ", "Five Nights at Freddy " ]
+
+for opiniao in jogos:
+    if opiniao == "Hollow Knight":
+        print ("Meu jogo favorito " + opiniao)
+    elif opiniao == "Five Nights at Freddy":
+        print (opiniao + "Como um jogo é horrivel, sem graça e nem sequer da medo")
+    else:
+        print (opiniao + "ok")
